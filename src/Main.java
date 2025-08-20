c785288ec6d74712d119fff2d69ac022c344648a
rrribeiro <gustavorgb4382@gmail.com>
2025-08-20 09:38:19 -0300

    Updated main class to remove unnecessary methods.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import com.formdev.flatlaf.FlatDarkLaf;
 import controller.TerminalController;
-import util.ConexaoBancoDados;
 import view.TelaAutenticacao;
-
-import java.sql.Connection;
-
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException; // Import específico
 
 /**
  * Classe principal que inicia a aplicação do Sistema de Controle de Acesso.
@@ -15,20 +7,8 @@ import javax.swing.UnsupportedLookAndFeelException; // Import específico
  */
 public class Main {
 
-    public static void main(String[] args) {
-        // SwingUtilities.invokeLater garante que a GUI seja criada e manipulada
-        // na thread correta (Event Dispatch Thread - EDT).
-        SwingUtilities.invokeLater(() -> {
-            try {
-                // Configura o Look and Feel ANTES de qualquer componente Swing ser criado.
-                UIManager.setLookAndFeel(new FlatDarkLaf());
-            } catch (UnsupportedLookAndFeelException e) {
-                // Se o tema falhar, a aplicação ainda rodará com o tema padrão do Java.
-                System.err.println("Falha ao inicializar o tema FlatLaf. Usando tema padrão.");
-                e.printStackTrace();
-            }
+    public static void main(String[] args) { 
 
-            Connection conexao = ConexaoBancoDados.getConexao();
             // 1. Cria a instância única do nosso controller.
             TerminalController controller = new TerminalController();
 
@@ -37,6 +17,6 @@ public class Main {
 
             // 3. Torna a tela visível.
             telaPrincipal.setVisible(true);
-        });
+        
     }
 }
