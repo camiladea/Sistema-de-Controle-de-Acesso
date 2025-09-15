eb9aff54db8d3b5066c3dd0aaf42948924fef77a
José Olávio da Silva Ronald Perin Rauta <jose.os2006@aluno.ifsc.edu.br>
2025-09-15 11:58:29 -0300

    Banco de dados arrumado

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,16 +1,10 @@
 package model;
 
-/**
- * Representa um funcionário no sistema, uma especialização da classe Usuario.
- * Contém informações específicas de funcionários, como o cargo.
- */
 public class Funcionario extends Usuario {
 
     private String cargo;
 
     /**
-     * Construtor para inicializar um objeto Funcionario.
-     *
      * @param nome       Nome completo do funcionário.
      * @param cpf        CPF do funcionário.
      * @param email      E-mail de contato.
@@ -21,8 +15,6 @@ public class Funcionario extends Usuario {
         super(nome, cpf, email, digitalFIR); // [cite: 311]
         this.cargo = cargo; // [cite: 311]
     }
-
-    // --- Getters e Setters ---
 
     public String getCargo() {
         return cargo; // [cite: 313]
